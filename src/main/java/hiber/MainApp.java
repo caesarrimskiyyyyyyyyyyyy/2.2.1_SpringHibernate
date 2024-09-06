675363f16fb95f277e8fcdcb3ad476ba9a6354a4
caesarrimskiyyyyyyyyyyyy <caesarrimskiy@gmail.com>
2024-09-06 21:52:49 +0300

    one-to-one relationship

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,34 +1,40 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
-import java.util.List;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
-      UserService userService = context.getBean(UserService.class);
+        UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+        Car carFirst = new Car(654, "MB");
+        Car carSecond = new Car(789, "BW");
+        Car carThird = new Car(122, "AD");
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
+        User user1 = new User("Tony", "Soprano", "panicattack@gmail.com");
+        User user2 = new User("Christopher", "Moltisanti", "babysoldier@gmail.com");
+        User user3 = new User("Silvio", "Dante", "outlawz@gmail.ru");
 
-      context.close();
-   }
+        user1.setCar(carFirst);
+        user2.setCar(carSecond);
+        user3.setCar(carThird);
+
+        userService.add(user1);
+        userService.add(user2);
+        userService.add(user3);
+
+        userService.listUsers().stream()
+                .map(entry -> String.format("%s : %s", entry.getFirstName(), entry.getCar().getModel()))
+                .forEach(System.out::println);
+
+        context.close();
+    }
 }
