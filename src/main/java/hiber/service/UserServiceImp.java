108afe01571e34b1458e3b69e8de96ff225e3b32
caesarrimskiyyyyyyyyyyyy <caesarrimskiy@gmail.com>
2024-09-13 19:33:01 +0300

    delete the entry

--- src/main/java/hiber/service/UserServiceImp.java
+++ src/main/java/hiber/service/UserServiceImp.java
@@ -30,7 +30,6 @@ public class UserServiceImp implements UserService {
         return userDao.listUsers();
     }
 
-    @Transactional
     @Override
     public User getUser(long series, String model) {
         return userDao.getUser(series, model);
